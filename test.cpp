5f35c13baa541a8d878d43075cd4388d8b391f9c
Ilnur Timergaliev <49416213+mhufflep@users.noreply.github.com>
2022-07-31 20:58:04 +0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,19 +6,15 @@ int main(int argc, char *argv[]) {
 		return 1;
 	
 	try {
-		//Converting filename to string
 		string filename(argv[1]);
 		
-		//Creating json object
+		// Creating json object
 		JSON::Parser parser(filename);
 
-		//Trying to load json file
-		parser.loadFile();
-
-		//Parsing whole file in json object
+		// Parsing whole file in json object
 		JSON::Object *obj = parser.parse();
 
-		//Using method get to access object fields and converting it to primitive type
+		// Using method get to access object fields and converting it to primitive type
 		// string keyString = obj->get("keyString")->toStr();
 		// double keyDouble = obj->get("keyNumber")->toNum();
 		
@@ -30,7 +26,7 @@ int main(int argc, char *argv[]) {
 		// bool keyBool1 = obj->get("keyBool1")->toBool();
 		// bool keyBool2 = obj->get("keyBool2")->toBool();
 		
-		//Printing received values
+		// Printing received values
 		// std::cout << "keyString: " << keyString << std::endl;
 		// std::cout << "keyDouble: " << keyDouble << std::endl;
 
@@ -41,7 +37,6 @@ int main(int argc, char *argv[]) {
 		// std::cout << "keyBool1: " << keyBool1 << std::endl;
 		// std::cout << "keyBool2: " << keyBool2 << std::endl;
 
-
 		string keyArr_value1 = obj->get("keyArr")->get(0)->toStr();
 		string keyArr_value2 = obj->get("keyArr")->get(1)->toStr();
 		string keyArr_value3 = obj->get("keyArr")->get(2)->toStr();
@@ -49,10 +44,6 @@ int main(int argc, char *argv[]) {
 		std::cout << "keyArr->value1: " << keyArr_value1 << std::endl;
 		std::cout << "keyArr->value2: " << keyArr_value2 << std::endl;
 		std::cout << "keyArr->value3: " << keyArr_value3 << std::endl;
-		
-		
-		string proxy = obj->get("proxytest")->get("proxy")->get("pass")->toStr();
-		std::cout << "pass: " << proxy << std::endl;
 
 		delete obj;	
 	} 
